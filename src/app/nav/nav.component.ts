ddc4d85bb11bc84730c849d8e012d1ab9ca91aaf
Norbert Csibi <cibi_norbert@yahoo.com>
2019-11-02 12:57:54 -0700

    if authent, certain bootstrap comp. will show

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,27 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
-
   navModel: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  login(){
+  login() {
     // we allways need to subscribe to observables
     // And we do something with that observable inside it
-    this.authService.login(this.navModel).subscribe(next => {
-      console.log('Logged in succesfully');
-    }, error => {
-      console.log('failed to login');
-    }); 
+    this.authService.login(this.navModel).subscribe(
+      next => {
+        console.log("Logged in succesfully");
+      },
+      error => {
+        console.log("failed to login");
+      }
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+  isLoggedIn() {
+    const token = localStorage.getItem('token');
+    // if is something in this token will return true, otherwise return false
+    return !!token;
   }
 }
