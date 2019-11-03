5cff69c5306109259c0ba14b8b2686e3e03cfdd7
Norbert Csibi <cibi_norbert@yahoo.com>
2019-11-03 12:01:10 -0800

    Added guard to the router / Fixed the 2nd tab open to redirect to the home page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,29 +3,40 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 // Array of routes
 // ORDERING IS IMPORTANT
 export const appRoutes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
   },
   {
-    path: 'members',
-    component: MemberListComponent
-  },
-  {
-    path: 'messages',
-    component: MessagesComponent
-  },
-  {
-    path: 'lists',
-    component: ListsComponent
+    // if we put something here, we can access the childs like e.g (localhost:4200/dummymembers).
+    // But empty means the childs can be accessed like /members
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'members',
+        component: MemberListComponent
+        // canActivate: [AuthGuard] specification of our guards. We have only 1 This needs to be added everywhere
+      },
+      {
+        path: 'messages',
+        component: MessagesComponent
+      },
+      {
+        path: 'lists',
+        component: ListsComponent
+      }
+    ]
   },
   {
     path: '**', // anything which doesn`t match what we have here, it will redirect to the home
-    redirectTo: 'home', // redirects to..
+    redirectTo: '', // redirects to..
     pathMatch: 'full' // we want to match the full path to the url
   }
 ];
